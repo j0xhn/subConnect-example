6070d6615d2951423a8d4de7d81685b478dcd7aa
John D. Storey <johndangerstorey@gmail.com>
2014-05-17 21:24:17 -0600

    Adds remove and add functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,11 @@
 'use strict';
 
 angular.module('subconnectApp')
-  .controller('MainCtrl', function ($scope, $firebase, Sov) {
+  .controller('MainCtrl', function ($scope, $firebase, Sov, $filter) {
+
+// Make scope variables
 ///////////////////////////
-// MAKE SCOPE VARIABLES
-///////////////////////////
+    $scope.Math = window.Math;
     var test = 'My Angular Is Working';
     $scope.test = test;
     $scope.testFunction = function(param){
@@ -12,13 +13,13 @@ angular.module('subconnectApp')
     }
     var subs = Sov.getSoV();
     subs.$bind($scope, 'subs');
-///////////////////////////
+
 // Firebase
 ///////////////////////////
     var sovRef = new Firebase('https://subconnect.firebaseio.com/');
     // Automatically syncs everywhere in realtime
     $scope.sov = $firebase(sovRef);
-///////////////////////////
+
 // Creating Subs
 ///////////////////////////
     // function for pushing new object
@@ -27,11 +28,24 @@ angular.module('subconnectApp')
       sovRef.push({
              "category" : categoryFromView || '',
              "subcategory":subcategoryFromView || '',
-             "amount": amountFromView || '',
+             "amount": amountFromView || 0,
              "accepted": '-',
              "comments" : ['first comment','second comment','third comment']
       });
     }
+
+    $scope.subDelete = function (sub){
+      var deleteRef = new Firebase('https://subconnect.firebaseio.com/'+sub.$id);
+      deleteRef.remove();
+      // console.log(sub.$id);
+      // sub.set(null);
+      // var deleteSub = sovRef+'/'+sub.$id;
+      // console.log(deleteSub);
+      // deleteSub.remove(onComplete);
+    }
+
+// Math for Subtotal
+///////////////////////////
 
 
   });
---
+++ app/scripts/filters/sumbykey.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('subconnectApp')
+  .filter('sumByKey', function () {
+    return function (data, key) {
+          console.log(data, key)
+        if (typeof (data) === 'undefined' || typeof (key) === 'undefined') {
+            return 0;
+        }
+        var sum = 0;
+        for (var i = data.length - 1; i >= 0; i--) {
+            sum += parseInt(data[i][key]);
+        }
+        return sum;
+    };
+  });
